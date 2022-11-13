db50e39e9c546f78fb7300bc249ab59db84e46db
Sagar Reddy <nsagarreddy3@gmail.com>
2022-11-13 11:59:43 +0530

    First Commit: Hash Algorithm page and Hash a Folder Page

---
+++ pages/Content-based_Image_Retrieval.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Setting page to utilize Full Body Size
+st.set_page_config(layout="wide")
+
+st.markdown("# <div style=\"text-align: center;\">Content-based Image Retrieval</div>", unsafe_allow_html=True)
+" "
+" "
